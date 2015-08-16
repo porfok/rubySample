18a01c0590a641288c90582020f16a18e92d54be
Por Fok <porfok33@gmail.com>
2015-08-16 14:13:09 +0800

    data types

---
+++ dataTypes.rb
@@ -0,0 +1,62 @@
+# Ruby Sample : Data Types
+# Version : Python 2.x
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+puts "Ruby Data Types"
+
+# Numbers
+puts
+puts "Numbers"
+a = 10
+puts "a = #{a}"
+
+# Strings
+puts
+puts "Strings"
+b = "This is a string."
+puts "b = #{b}"
+
+# Lists
+puts
+puts "Lists"
+
+c = [1, 2, 4, 5, 7, 10]
+puts "c = #{c}"
+
+d = ["Mary", "John", "Joe"]
+puts "d = #{d}"
+
+# List substraction
+puts
+puts "List substraction"
+
+e = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+f = [2, 4, 6, 8, 10]
+g = e - f
+puts "g = #{g}"
+
+# Push of List
+puts
+puts "Push of List"
+
+h = []
+h.push("Johnson")
+h.push("Peter")
+puts "h = #{h}"
+
+# Dictionaries
+# Tuples
+# Boolean
+# Set
